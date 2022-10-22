e91b63d9e02603ffc42eed2b7475b9cf58002214
jinvisible <s8746@naver.com>
2022-10-23 00:15:47 +0900

    minor change

--- developer/Unit03/Game/DrawJumper.cs
+++ developer/Unit03/Game/DrawJumper.cs
@@ -3,21 +3,62 @@ using System.Collections.Generic;
 
 namespace Unit03.Game {
     public class DrawJumper {
+        private string line1 = "  ---";
+        private string line2 = " /   \\";
+        private string line3 = " \\   /";
+        private string line4 = "  \\ /";
+        private string line50 = "   0";
+        private string line5X = "   X";
+        private string line6 = "  /|\\";
+        private string line7 = "  / \\";
+        private string line8 = "^^^^^^^";
         public void ShowJumper(int attempt) {
             if (attempt == 4) {
-                Console.WriteLine("  ---\n /   \\\n  ---\n \\   /\n  \\ /\n   0\n  /|\\\n  / \\\n\n^^^^^^^");
+                Console.WriteLine(line1);
+                Console.WriteLine(line2);
+                Console.WriteLine(line1);
+                Console.WriteLine(line3);
+                Console.WriteLine(line4);
+                Console.WriteLine(line50);
+                Console.WriteLine(line6);
+                Console.WriteLine(line7);
+                Console.WriteLine();
+                Console.WriteLine(line8);
             }
             if (attempt == 3) {
-                Console.WriteLine(" /   \\\n  ---\n \\   /\n  \\ /\n   0\n  /|\\\n  / \\\n\n^^^^^^^");
+                Console.WriteLine(line2);
+                Console.WriteLine(line1);
+                Console.WriteLine(line3);
+                Console.WriteLine(line4);
+                Console.WriteLine(line50);
+                Console.WriteLine(line6);
+                Console.WriteLine(line7);
+                Console.WriteLine();
+                Console.WriteLine(line8);
             }
             if (attempt == 2) {
-                Console.WriteLine(" \\   /\n  \\ /\n   0\n  /|\\\n  / \\\n\n^^^^^^^");
+                Console.WriteLine(line3);
+                Console.WriteLine(line4);
+                Console.WriteLine(line50);
+                Console.WriteLine(line6);
+                Console.WriteLine(line7);
+                Console.WriteLine();
+                Console.WriteLine(line8);
             }
             if (attempt == 1) {
-                Console.WriteLine("  \\ /\n   0\n  /|\\\n  / \\\n\n^^^^^^^");
+                Console.WriteLine(line4);
+                Console.WriteLine(line50);
+                Console.WriteLine(line6);
+                Console.WriteLine(line7);
+                Console.WriteLine();
+                Console.WriteLine(line8);
             }
             if (attempt == 0) {
-                Console.WriteLine("   X\n  /|\\\n  / \\\n\n^^^^^^^");
+                Console.WriteLine(line5X);
+                Console.WriteLine(line6);
+                Console.WriteLine(line7);
+                Console.WriteLine();
+                Console.WriteLine(line8);
             }
         }
 
